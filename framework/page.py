7c3c456b99935c0e08588869b95be459f06c69da
Dmytry Bondariev <bondarievmd@gmail.com>
2023-09-06 19:40:55 +0300

    Started defining methods for Page class

--- framework/page.py
+++ framework/page.py
@@ -3,8 +3,8 @@ class Page:
     def __init__(self, driver):
         self.driver = driver
 
-    def find_element(self):
-        raise NotImplementedError
+    def find_element(self, *args, **kwargs):
+        raise self.driver.find_element(*args, **kwargs)
 
-    def click_element(self):
-        raise NotImplementedError
+    def click_element(self, *args, **kwargs):
+        return self.driver.click_element(*args, **kwargs)
