8ac8542a07a64cfa03f0b3347305ce319f524179
Dmytry Bondariev <bondarievmd@gmail.com>
2023-09-08 11:51:16 +0300

    Moved tests to corresponding file

--- tests/login/test_login.py
+++ tests/login/test_login.py
@@ -1,2 +1,46 @@
-def test_user_login(user_login_fixture):
-    assert True
+import time
+
+import pytest
+from appium.webdriver.common.appiumby import AppiumBy
+from selenium.common import NoSuchElementException
+
+import dev_in_test_app_team.utils.elements_xpath as xpath
+
+
+def assert_login_successful(login_page):
+    assert login_page.find_element(
+        AppiumBy.XPATH,
+        xpath.burger_menu_xpath
+    ) is not None
+
+
+def assert_login_failed(login_page):
+    with pytest.raises(NoSuchElementException):
+        login_page.find_element(
+            AppiumBy.XPATH,
+            xpath.burger_menu_xpath
+        )
+
+
+@pytest.mark.parametrize("email, password, expected_result", [
+    ("qa.ajax.app.automation@gmail.com", "qa_automation_password", "success"),
+    ("invalid_email@example.com", "qa_automation_password", "failure"),
+    ("qa.ajax.app.automation@gmail.com", "invalid_password", "failure"),
+    ("qa.ajax.app.automation@gmail.com", "  ", "failure"),
+    ("  ", "qa_automation_password", "failure"),
+])
+def test_user_login(user_login_fixture, email, password, expected_result):
+    login_page = user_login_fixture
+    login_page.click_element(login_page.find_login_button())
+    login_page.enter_email(login_page.find_email_field(), email)
+    login_page.enter_password(login_page.find_password_field(), password)
+    login_page.click_element(login_page.find_login_button())
+
+    time.sleep(4)
+
+    if expected_result == "success":
+
+        assert_login_successful(login_page)
+    elif expected_result == "failure":
+
+        assert_login_failed(login_page)
