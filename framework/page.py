edc72a475b13c1b323d1d8dedb0297256d9cff0d
Dmytry Bondariev <bondarievmd@gmail.com>
2023-09-08 10:47:50 +0300

    Moved xpath to separate file; put Page class implementation to corresponding file

--- framework/page.py
+++ framework/page.py
@@ -4,7 +4,8 @@ class Page:
         self.driver = driver
 
     def find_element(self, *args, **kwargs):
-        raise self.driver.find_element(*args, **kwargs)
+        return self.driver.find_element(*args, **kwargs)
 
-    def click_element(self, *args, **kwargs):
-        return self.driver.click_element(*args, **kwargs)
+    @staticmethod
+    def click_element(element):
+        element.click()
---
+++ utils/elements_xpath.py
@@ -0,0 +1,10 @@
+login_button_xpath = '//android.widget.TextView[@text="Log In"]'
+
+email_field_xpath = '//android.widget.FrameLayout[1]/androidx.compose.ui.platform.ComposeView/' \
+                    'android.view.View/android.widget.EditText'
+
+password_field_xpath = "//android.widget.FrameLayout[2]/androidx.compose.ui.platform.ComposeView/" \
+                       "android.view.View/android.widget.EditText"
+
+burger_menu_xpath = "//android.widget.LinearLayout/android.view.ViewGroup/" \
+                    "android.widget.FrameLayout/android.widget.ImageView"
